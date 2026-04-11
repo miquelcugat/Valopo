be2ca6125bb95de524a16e7f46c7840f3bef4b16
miquelcugat <cugat23@gmail.com>
2026-04-11 16:31:36 +0200

    Update checkout.js

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -1,6 +1,5 @@
 import { stripe } from '../../../lib/stripe';
 import { supabaseAdmin } from '../../../lib/supabaseAdmin';
-import { createServerClient } from '@supabase/auth-helpers-nextjs';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
